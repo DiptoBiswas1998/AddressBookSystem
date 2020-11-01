5eb97d13de331f1e1a54efe17393a7f338ba4e67
unknown <diptobiswas1998@gmail.com>
2020-11-01 23:56:57 +0530

    [Dipto] refactor: Adding contacts into the address book

---
+++ UC3_InsertContacts.sql
@@ -0,0 +1,3 @@
+INSERT INTO address_book VALUES
+('Dipto', 'Biswas', 'Rahara', 'Kolkata', 'West Bengal', 700118, 9876543210, 'diptobiswas@gmail.com'),
+('Ben', 'Ten', 'Vashi', 'Mumbai', 'Maharashtra', 400073, 9977553311, 'ben10@gmail.com')

e6ed1835f55e0085e21e120d877cb7096276ca67
unknown <diptobiswas1998@gmail.com>
2020-11-02 00:15:30 +0530

    [Dipto] refactor: Modifying UC3

--- UC3_InsertContacts.sql
+++ UC3_InsertContacts.sql
@@ -1,3 +1,4 @@
 INSERT INTO address_book VALUES
-('Dipto', 'Biswas', 'Rahara', 'Kolkata', 'West Bengal', 700118, 9876543210, 'diptobiswas@gmail.com'),
-('Ben', 'Ten', 'Vashi', 'Mumbai', 'Maharashtra', 400073, 9977553311, 'ben10@gmail.com')
+('Dipto', 'Biswas', 'Rahara', 'Kolkata', 'West Bengal', 700, 98, 'diptobiswas@gmail.com'),
+('Ben', 'Ten', 'Vashi', 'Mumbai', 'Maharashtra', 400, 99, 'ben10@gmail.com')
+

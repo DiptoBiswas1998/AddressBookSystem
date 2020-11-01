46bdc4330e9e32a1a44c34d5d5b296c3c156c024
unknown <diptobiswas1998@gmail.com>
2020-11-02 00:08:48 +0530

    [Dipto] refactor: Retrieve person info from city or state name

---
+++ UC6_RetrievePersonFromCityOrState.sql
@@ -0,0 +1,4 @@
+INSERT INTO address_book VALUES
+('Ash', 'Ketchum', 'PalletTown', 'Guwahati', 'Assam', 900, 88, 'ashketchum@gmail.com')
+SELECT * FROM address_book WHERE city = 'Mumbai'
+SELECT * FROM address_book WHERE state = 'Assam'
